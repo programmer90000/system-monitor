942a35b5fa6730212b4ae9b1474e22f8a316431e
Abdul Rehman <abdulr568programming@outlook.com>
2025-10-09 13:23:41 +0100

    feat: Run view_system_logs function

--- frontend/src/screens/Logs.jsx
+++ frontend/src/screens/Logs.jsx
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { runCommand } from "../lib/run-commands.js";
 
 const Logs = () => {
+    const [systemLogs, setSystemLogs] = useState();
+
+    const hasRunRef = useRef(false);
+
+    useEffect(() => {
+        if (!hasRunRef.current) {
+            hasRunRef.current = true;
+
+            Promise.allSettled([
+                runCommand("view_system_logs", []).then((output) => {
+                    setSystemLogs(output);
+                    return { "type": "systemLogs", "value": output };
+                }),
+
+            ]).then((results) => {
+
+                results.forEach((result, index) => {
+                    if (result.status === "fulfilled") {
+                        console.log(result);
+                    }
+                    if (result.status === "rejected") {
+                        console.error(`Command ${index} failed:`, result.reason);
+                    }
+                });
+            });
+        }
+    }, []);
+
     return (
         <div/>
     );
